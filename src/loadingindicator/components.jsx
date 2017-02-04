34c9a1693822bf074143b8a3f8ad68c3cb8c8ddb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-02-04 20:48:08 +0100

    Timer on the loading indicator to show it only after a few ms

--- src/loadingindicator/components.jsx
+++ src/loadingindicator/components.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 
 
-export function LoadingIndicator({ active }) {
-  if (active) {
-    return (
-      <p><b>Loading&hellip;</b></p>
-    )
+export class LoadingIndicator extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+    };
+
+    this.timer = null;
   }
 
-  return null;
+  componentDidMount() {
+    this.syncShowState();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.active !== nextProps.active || this.state.show !== nextState.show;
+  }
+
+  componentDidUpdate() {
+    this.syncShowState();
+  }
+
+  syncShowState() {
+    if (this.props.active) {
+      if (this.timer === null) {
+        this.timer = setTimeout(() => {
+          this.setState({
+            show: true,
+          });
+        }, 250);
+      }
+    } else {
+      if (this.timer) {
+        clearTimeout(this.timer);
+        this.timer = null;
+      }
+      this.setState({
+        show: false,
+      });
+    }
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <p><b>Loading&hellip;</b></p>
+      )
+    }
+
+    return null;
+  }
+
 }
 
 LoadingIndicator.propTypes = {
