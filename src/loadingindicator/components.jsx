8bebff544e7d3eebf45600c807c9d511c0ebc2c2
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-02-05 14:55:27 +0100

    Pure loading indicator

--- src/loadingindicator/components.jsx
+++ src/loadingindicator/components.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export class LoadingIndicator extends React.Component {
+export class LoadingIndicator extends React.PureComponent {
 
   constructor(props) {
     super(props);
